420cb6e6f792df0096f1f6d12a4c09a22f3057fc
yablko <yablko@brm.sk>
2022-01-21 15:10:40 +0100

    04-KEYBOARD

---
+++ js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('keydown', (event) => {
+
+	if (event.key === 'Enter') {
+		submitWord()
+	}
+	else if (event.key === 'Backspace') {
+		removeLetter()
+	}
+	else {
+		addLetter(event.key)
+	}
+
+})
