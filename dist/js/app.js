230bda816b993f41a6a3d9af4f20a0bdc14230b2
yablko <yablko@brm.sk>
2022-02-11 13:36:28 +0100

    fixed double-enter bug
    befure, if you quickly pressed enter while tile reveal animations were running, game would skip rows

--- dist/js/app.js
+++ dist/js/app.js
@@ -14,161 +14,169 @@ let tries = 1
 let noAccentWords = allWords.map((x) => noAccents(x))
 let noAccentSolution = noAccents(solution)
 
+let canSubmit = true
+
 let lettersInRow = {
-	correct: [],
-	present: [],
-	wrong: [],
+    correct: [],
+    present: [],
+    wrong: [],
 }
 
 // TILE TO UPDATE
 const currentTile = () => {
-	return currentRow().querySelector(`:nth-child(${word.length})`)
+    return currentRow().querySelector(`:nth-child(${word.length})`)
 }
 
 // ROW WE'RE PLAYING IN
 const currentRow = () => {
-	return document.querySelector(`.board .row:nth-of-type(${tries})`)
+    return document.querySelector(`.board .row:nth-of-type(${tries})`)
 }
 
 // KEY PRESS
 document.addEventListener("keydown", (event) => {
-	// de-focus any active element (needed for micromodal)
-	if ("activeElement" in document) document.activeElement.blur()
+    // de-focus any active element (needed for micromodal)
+    if ("activeElement" in document) document.activeElement.blur()
 
-	if (event.key === "Enter") {
-		submitWord()
-	} else if (event.key === "Backspace") {
-		removeLetter()
-	} else {
-		addLetter(event.key)
-	}
+    if (event.key === "Enter") {
+        submitWord()
+    } else if (event.key === "Backspace") {
+        removeLetter()
+    } else {
+        addLetter(event.key)
+    }
 })
 
 // SUBMIT
 const submitWord = () => {
-	if (word.length < maxWordLength) return
+    if (word.length < maxWordLength) return
 
-	// is this a real word ?
-	if (!noAccentWords.includes(noAccents(word))) {
-		animateRowShake(currentRow())
-		tooltipUnknownWord()
-		return
-	}
+    // can't submit again, while animations are running
+    if (!canSubmit) return
+    canSubmit = false
 
-	findLettersInRow()
-	highlightLetters()
-	animateTileReveal(currentRow())
+    // is this a real word ?
+    if (!noAccentWords.includes(noAccents(word))) {
+        animateRowShake(currentRow())
+        tooltipUnknownWord()
+        return
+    }
 
-	// wait for reveal animation to finish
-	setTimeout(judgeResult, 1500)
+    findLettersInRow()
+    highlightLetters()
+    animateTileReveal(currentRow())
+
+    // wait for reveal animation to finish
+    setTimeout(judgeResult, 1500)
 }
 
 // ADD LETTER
 const addLetter = (character) => {
-	if (word.length >= maxWordLength) return
+    if (word.length >= maxWordLength) return
 
-	// allow only letters
-	if (/^\p{L}$/u.test(character)) {
-		word = word + character
-		word = word.toLowerCase()
+    // allow only letters
+    if (/^\p{L}$/u.test(character)) {
+        word = word + character
+        word = word.toLowerCase()
 
-		let tile = currentTile()
-		tile.innerHTML = character
+        let tile = currentTile()
+        tile.innerHTML = character
 
-		animateTileBounce(tile)
-	}
+        animateTileBounce(tile)
+    }
 
-	// console.log(word)
+    // console.log(word)
 }
 
 // REMOVE LETTER
 const removeLetter = () => {
-	if (word.length <= 0) return
+    if (word.length <= 0) return
 
-	let tile = currentTile()
-	tile.innerHTML = ""
-	tile.className = "tile"
+    let tile = currentTile()
+    tile.innerHTML = ""
+    tile.className = "tile"
 
-	word = word.slice(0, -1)
+    word = word.slice(0, -1)
 }
 
 // JUDGE RESULT
 const judgeResult = () => {
-	if (noAccents(word) === noAccentSolution) {
-		youWin()
-	} else if (tries >= maxTries) {
-		youLose()
-	} else {
-		youTryAgain()
-	}
+    canSubmit = true
+
+    if (noAccents(word) === noAccentSolution) {
+        youWin()
+    } else if (tries >= maxTries) {
+        youLose()
+    } else {
+        youTryAgain()
+    }
 }
 
 // YOU WIN
 const youWin = () => {
-	animateTileDance(currentRow())
-	setTimeout(() => confetti(), 1500)
-	tooltipHellYeah()
-	fadeOutKeyboard()
+    animateTileDance(currentRow())
+    setTimeout(() => confetti(), 1500)
+    tooltipHellYeah()
+    fadeOutKeyboard()
 }
 
 // YOU LOSE
 const youLose = () => {
-	dropTheBoard()
-	fadeOutKeyboard(1600)
+    dropTheBoard()
+    fadeOutKeyboard(1600)
 }
 
 // YOU TRY AGAIN
 const youTryAgain = () => {
-	word = ""
-	tries++
+    word = ""
+    tries++
 }
 
 // FIND LETTERS IN ROW
 const findLettersInRow = () => {
-	let present = []
-	let correct = []
-	let wrong = []
+    let present = []
+    let correct = []
+    let wrong = []
 
-		;[...word].forEach((letter, index) => {
-			letter = noAccents(letter)
+        ;[...word].forEach((letter, index) => {
+            letter = noAccents(letter)
 
-			// letter in correct place
-			if (letter === noAccentSolution.charAt(index)) {
-				correct.push(letter)
-			}
-			// letter in wrong place
-			else if (noAccentSolution.includes(letter)) {
-				present.push(letter)
-			}
-			// wrong letter
-			else {
-				wrong.push(letter)
-			}
-		})
+            // letter in correct place
+            if (letter === noAccentSolution.charAt(index)) {
+                correct.push(letter)
+            }
+            // letter in wrong place
+            else if (noAccentSolution.includes(letter)) {
+                present.push(letter)
+            }
+            // wrong letter
+            else {
+                wrong.push(letter)
+            }
+        })
 
-	lettersInRow = {
-		present,
-		correct,
-		wrong,
-	}
+    lettersInRow = {
+        present,
+        correct,
+        wrong,
+    }
 }
 
 // MOBILE
 const keyboard = document.querySelector('.keyboard')
 keyboard.addEventListener('click', (event) => {
-	if (event.target.nodeName !== 'BUTTON') return
-	let character = event.target.id
+    if (event.target.nodeName !== 'BUTTON') return
+    let character = event.target.id
 
-	if (character === 'again') {
-		window.location.reload()
-	}
-	else if (character === '↵') {
-		submitWord()
-	}
-	else if (character === '←') {
-		removeLetter()
-	}
-	else {
-		addLetter(character)
-	}
+    if (character === 'again') {
+        window.location.reload()
+    }
+    else if (character === '↵') {
+        submitWord()
+    }
+    else if (character === '←') {
+        removeLetter()
+    }
+    else {
+        addLetter(character)
+    }
 })
--- dist/js/tooltips.js
+++ dist/js/tooltips.js
@@ -5,43 +5,43 @@
 const tooltipElement = document.querySelector('.tooltip')
 
 const tooltip = (text) => {
-	if (!text) return
+    if (!text) return
 
-	tooltipElement.innerHTML = text
-	tooltipElement.className = 'tooltip animate__animated'
+    tooltipElement.innerHTML = text
+    tooltipElement.className = 'tooltip animate__animated'
 
-	setTimeout(() => {
-		tooltipElement.classList.add('animate__fadeIn')
-	}, 0)
+    setTimeout(() => {
+        tooltipElement.classList.add('animate__fadeIn')
+    }, 0)
 
-	setTimeout(() => {
-		tooltipElement.className = 'tooltip animate__animated'
-		tooltipElement.classList.add('animate__fadeOut')
-	}, 2500)
+    setTimeout(() => {
+        tooltipElement.className = 'tooltip animate__animated'
+        tooltipElement.classList.add('animate__fadeOut')
+    }, 2500)
 }
 
 // UNKNOWN WORD TOOLTIP
 const tooltipUnknownWord = () => {
-	let lastLetter = word.slice(-1).toLowerCase()
+    let lastLetter = word.slice(-1).toLowerCase()
 
-	// if (lastLetter === 'y') {
-	// 	tooltip("bez množných čísel")
-	// }
+    // if (lastLetter === 'y') {
+    // 	tooltip("bez množných čísel")
+    // }
 
-	if (['y', 'i', 't'].includes(lastLetter)) {
-		tooltip("žiadne prídavné mená, slovesá, množné čísla")
-	}
-	else {
-		tooltip('to slovo nepoznám')
-	}
+    if (['y', 'i', 't'].includes(lastLetter)) {
+        tooltip("žiadne prídavné mená, slovesá, množné čísla")
+    }
+    else {
+        tooltip('to slovo nepoznám')
+    }
 }
 
 // TOOLTIP - YOU WON
 const tooltipHellYeah = () => {
-	setTimeout(() => {
-		if (tries === 1) tooltip('WOW!')
-		else if (tries === 2 || tries === 3) tooltip('PARÁDA!')
-		else if (tries === 4 || tries === 5) tooltip('máš to!')
-		else if (tries === 6) tooltip('uff, tesnotka!')
-	}, 1500)
+    setTimeout(() => {
+        if (tries === 1) tooltip('WOW!')
+        else if (tries === 2 || tries === 3) tooltip('PARÁDA!')
+        else if (tries === 4 || tries === 5) tooltip('máš to!')
+        else if (tries === 6) tooltip('uff, tesnotka!')
+    }, 1500)
 }
